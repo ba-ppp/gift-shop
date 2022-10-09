7fa530009f7a7495f7d15d0cc370800e8fda951d
khangb1910652 <nguyenduykhang444@gmail.com>
2022-10-09 16:16:52 +0700

    service/product

--- server/src/controllers/Product/getProducts.ts
+++ server/src/controllers/Product/getProducts.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { knex } from "database/knex";
+import { idText } from "typescript";
 
 const ApiError = require('../../api-error');
 const router = express.Router();
@@ -10,11 +11,72 @@ export const getProduct = () => {
       
       async (req: express.Request, res: express.Response, next: express.NextFunction) => {
         try {
-            const products = await knex('product_detail')
+            let listItem = {
+                id_cat: "",
+                items: [] as any
+            }
+            let data = {
+                id_product: "",
+                id: "",
+                name: "",
+                size: [] as any,
+                price: [] as any,
+                describes: "",
+                status: "",
+                color: "",
+                image: ""
+            }
+            let products = [] as any;
+
+            const results = await knex('product_detail')
             .join('product', 'product.id_product', '=', 'product_detail.id_product')
-            .join('color_product', 'color_product.color', '=', 'product_detail.color')
-            .join('size_product', 'size_product.size', '=', 'product_detail.size')
             .select('*');
+
+            let currentProduct = results[0].id_product;
+            let currentCat = results[0].id_cat;
+            results.forEach((item: any) =>{
+                if(item.id_cat === currentCat){
+                    listItem.id_cat = item.id_cat;
+                    if(item.id_product === currentProduct){
+                        data.id_product = item.id_product
+                        data.id = item.id_detail;
+                        data.name = item.name_product;
+                        data.describes = item.describes;
+                        data.status = item.stt;
+                        data.color = item.color;
+                        data.image = item.image
+                        data.size.push(item.size);
+                        data.price.push(item.price);
+                        
+                    }else {
+                        listItem.items.push(data);
+                        currentProduct = item.id_product;
+                        const newData = {
+                            id_product : item.id_product,
+                            id : item.id_detail,
+                            name : item.name_product,
+                            describes : item.describes,
+                            status : item.stt,
+                            color : item.color,
+                            image : item.image,
+                            size : [item.size],
+                            price: [item.price],
+                        };
+                        data = {...newData}
+                    }
+                    
+                    // listItem.items.push(data);
+                }else{
+                    products.push(listItem);
+                    currentCat = item.id_cat;
+                    const newList = {
+                        id_cat : item.id_cat,
+                        items : [data]
+                    }
+                    listItem = {...newList};
+                }
+            })
+            products.push(listItem);
             res.send(products);
         } catch (error) {
         console.log(error);
