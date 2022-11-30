4cb7328f0040c40ccb5dc2846058a05542863a33
khangb1910652 <nguyenduykhang444@gmail.com>
2022-11-30 13:28:55 +0700

    khang

--- server/src/database/knex.ts
+++ server/src/database/knex.ts
@@ -4,7 +4,7 @@ export const knex = require('knex')({
       host : '127.0.0.1',
       port : 3306,
       user : 'root',
-      password : '1',
+      password : '12345',
       database : 'teddy_bears'
     }
   });
